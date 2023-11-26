2d63616dbe919e35f25a7d0482accb2abd48b3c3
WhyK <windchimeyk@gmail.com>
2023-11-26 15:39:38 +0900

    refactor(api): add error handling

--- routes/index.tsx
+++ routes/index.tsx
@@ -10,12 +10,16 @@ type HandlerProps = {
 
 export const handler: Handlers<HandlerProps> = {
   async GET(req, ctx) {
-    console.log(req.url);
-    const res = await fetch(`${new URL(req.url).origin}/api/report`);
-    console.log(res);
-    const reports: ReportMetaData[] = await res.json();
-    console.log(reports);
-    return ctx.render({ reports });
+    try {
+      console.log(req.url);
+      const res = await fetch(`${new URL(req.url).origin}/api/report`);
+      console.log(res);
+      const reports: ReportMetaData[] = await res.json();
+      console.log(reports);
+      return ctx.render({ reports });
+    } catch (_) {
+      return ctx.render({ reports: [] });
+    }
   },
 };
 
@@ -44,33 +48,35 @@ export default function Home({ data, url }: PageProps<HandlerProps>) {
         <Heading level={2}>各回の内訳</Heading>
 
         <div class="mt-3 flex flex-col gap-3">
-          {reports.map((data, index) => {
-            const count = index + 1;
-            return (
-              <section
-                class="rounded border(gray-500 2)"
-                key={data?.meta.date}
-              >
-                <a
-                  class="block px-3 py-4 hover:bg-gray-100 dark:hover:bg-gray-700"
-                  href={data?.path}
+          {reports.length !== 0
+            ? reports.map((data, index) => {
+              const count = index + 1;
+              return (
+                <section
+                  class="rounded border(gray-500 2)"
+                  key={data?.meta.date}
                 >
-                  <Heading level={3}>{`第${count}回`}</Heading>
-                  <p>{`第${count}回`}の内訳は以下の通りです。</p>
-                  <ul class="mt-3 flex flex-wrap gap-3">
-                    {extractHeading(data?.content)?.map((item) => {
-                      if (item.level === 1) return;
-                      return (
-                        <li class="bg-gray-200 dark:bg-gray-600 rounded px-3 py-1">
-                          {item.title}
-                        </li>
-                      );
-                    })}
-                  </ul>
-                </a>
-              </section>
-            );
-          })}
+                  <a
+                    class="block px-3 py-4 hover:bg-gray-100 dark:hover:bg-gray-700"
+                    href={data?.path}
+                  >
+                    <Heading level={3}>{`第${count}回`}</Heading>
+                    <p>{`第${count}回`}の内訳は以下の通りです。</p>
+                    <ul class="mt-3 flex flex-wrap gap-3">
+                      {extractHeading(data?.content)?.map((item) => {
+                        if (item.level === 1) return;
+                        return (
+                          <li class="bg-gray-200 dark:bg-gray-600 rounded px-3 py-1">
+                            {item.title}
+                          </li>
+                        );
+                      })}
+                    </ul>
+                  </a>
+                </section>
+              );
+            })
+            : <p>なんらかの理由でデータが取得できていないようです</p>}
         </div>
       </section>
       <section class="mt-12">
