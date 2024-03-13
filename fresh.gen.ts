6a7b9c2eb046593899da661b4b1d29a2fb165feb
WhyK <windchimeyk@gmail.com>
2024-03-13 18:31:01 +0900

    chore: update deps

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -2,22 +2,24 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $0 from "./routes/_404.tsx";
-import * as $1 from "./routes/_app.tsx";
-import * as $2 from "./routes/gfm.css.ts";
-import * as $3 from "./routes/index.tsx";
-import * as $4 from "./routes/report/[count].tsx";
+import * as $_404 from "./routes/_404.tsx";
+import * as $_app from "./routes/_app.tsx";
+import * as $gfm_css from "./routes/gfm.css.ts";
+import * as $index from "./routes/index.tsx";
+import * as $report_count_ from "./routes/report/[count].tsx";
+
+import { type Manifest } from "$fresh/server.ts";
 
 const manifest = {
   routes: {
-    "./routes/_404.tsx": $0,
-    "./routes/_app.tsx": $1,
-    "./routes/gfm.css.ts": $2,
-    "./routes/index.tsx": $3,
-    "./routes/report/[count].tsx": $4,
+    "./routes/_404.tsx": $_404,
+    "./routes/_app.tsx": $_app,
+    "./routes/gfm.css.ts": $gfm_css,
+    "./routes/index.tsx": $index,
+    "./routes/report/[count].tsx": $report_count_,
   },
   islands: {},
   baseUrl: import.meta.url,
-};
+} satisfies Manifest;
 
 export default manifest;
